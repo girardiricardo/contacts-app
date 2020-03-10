f1d3d2c2a0c124359c6fcbeff7fd4c24c0c61dbf
Ricardo Girardi <girardiricardo@outlook.com>
2020-03-09 22:48:18 -0300

    Adding contacts list component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+class ContractList extends React.Component {
+  render() {
+    const people = this.props.contacts;
+
+    return (
+      <ol>
+        {people.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ol>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContractList contacts={[{ name: "Tyler" }, { name: "Karen" }]} />
+      <ContractList contacts={[{ name: "Mikenzi" }, { name: "Amanda" }]} />
+      <ContractList contacts={[{ name: "Karen" }, { name: "Richard" }]} />
     </div>
   );
 }
